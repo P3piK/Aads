8e98aa78784174b2e989202fe0c27c2fb96e71d6
Pete <pete@peter.com>
2018-04-19 20:15:24 +0200

    Add code from Lab3

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab2aads
+namespace Lab2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numberOfElements = 64000;
+            int numberOfElements = 2048000;
             List<int> data = new List<int>();
             GenerateData(data, numberOfElements);
 
@@ -19,17 +19,99 @@ namespace Lab2aads
             stopWatch.Start();
 
             /* Sorting methods */
-
-            SelectionSort(data);
-            //InsertionSort(data);
-            //InsertionSortWithGuard(data);
-            //BubbleSort(data);
-            //CocktailSort(data);
+            //ShellSort(data, 3);
+            //ShellSort(data, 2);
+            //QuickSort1(data, 0, data.Count - 1);        // first element
+            //QuickSort2(data, 0, data.Count - 1);        // random
+            QuickSort3(data, 0, data.Count - 1);          // median of three
 
             stopWatch.Stop();
             Console.WriteLine("time: " + stopWatch.ElapsedMilliseconds + " [ms]");
 
             PrintResultMessage(data);
+        }
+
+        private static void QuickSort1(List<int> data, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = data[left];
+            while (i < j)
+            {
+                while (data[i] < pivot) i++;
+                while (data[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                QuickSort1(data, left, j);
+            }
+            if (i < right)
+            {
+                QuickSort1(data, i, right);
+            }
+        }
+        private static void QuickSort2(List<int> data, int left, int right)
+        {
+            Random random = new Random();
+            int i = left;
+            int j = right;
+            int pivot = data[random.Next(left, right + 1)];
+            while (i < j)
+            {
+                while (data[i] < pivot) i++;
+                while (data[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                QuickSort2(data, left, j);
+            }
+            if (i < right)
+            {
+                QuickSort2(data, i, right);
+            }
+        }
+        private static void QuickSort3(List<int> data, int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private static void ShellSort(List<int> data, int k)
+        {
+            int h = 1;
+            int n = data.Count;
+
+            while (h < n / Math.Pow(k, 2.0))
+            {
+                h = k * h + 1;
+            }
+            while (h > 0)
+            {
+                for(int i = h; i < n; i++)
+                {
+                    int x = data[i];
+                    int j = i;
+                    while (j >= h && x < data[j-h])
+                    {
+                        data[j] = data[j - h];
+                        j -= h;
+                    }
+                    data[j] = x;
+                }
+                h = h / k;
+            }
         }
 
         static void GenerateData(List<int> data, int numberOfElements)
@@ -41,128 +123,26 @@ namespace Lab2aads
             }
         }
 
-        static void SelectionSort(List<int> data)
-        {
-            int n = data.Count;
-            for (int i = 0; i < n; i++)
-            {
-                int k = i;
-                int minValue = data[i];
-
-                FindMinElement(data, n, i, ref k, ref minValue);
-
-                data[k] = data[i];
-                data[i] = minValue;
-            }
-        }
-
-        static void InsertionSort(List<int> data)
-        {
-            for (int i = 1; i < data.Count; i++)
-            {
-                int x = data[i];
-                int j = i - 1;
-                while (j >= 0 && x < data[j])
-                {
-                    data[j + 1] = data[j];
-                    j--;
-                }
-                data[j + 1] = x;
-            }
-        }
-
-        static void InsertionSortWithGuard(List<int> data)
-        { 
-            for (int i = 1; i < data.Count; i++)
-            {
-                int x = data[i];
-                int guard = x;
-                int j = i - 1;
-                while (j > 0 && x < data[j])
-                {
-                    data[j + 1] = data[j];
-                    j--;
-                }
-                data[j + 1] = x;
-            }
-        }
-
-        static void BubbleSort(List<int> data)
-        {
-            for(int i = 0; i < data.Count; i++)
-            {
-                for(int j = 1; j < data.Count; j++)
-                {
-                    if(data[j-1] > data[j])
-                    {
-                        Swap(data, j - 1, j);
-                    }
-                }
-            }
-        }
-        
-        static void CocktailSort(List<int> data)
-        {
-            bool swapped;
-            do
-            {
-                swapped = false;
-                for(int i = 0; i <= data.Count - 2; i++)
-                {
-                    if(data[i] > data[i+1])
-                    {
-                        Swap(data, i, i + 1);
-                        swapped = true;
-                    }
-                }
-                if(!swapped)
-                {
-                    break;
-                }
-                swapped = false;
-                for(int i = data.Count - 2; i >=0; i--)
-                {
-                    if(data[i] > data[i+1])
-                    {
-                        Swap(data, i, i + 1);
-                        swapped = true;
-                    }
-                }
-
-            } while (swapped);
-
-        }
-
-        static bool IsSorted(List<int> data)
-        {
-            for (int i = 1; i < data.Count; i++)
-            {
-                if (data[i - 1] > data[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
 
         #region private
-        private static void Swap(IList<int> list, int indexA, int indexB)
+        private static void Swap(List<int> list, int indexA, int indexB)
         {
             int tmp = list[indexA];
             list[indexA] = list[indexB];
             list[indexB] = tmp;
         }
-        private static void FindMinElement(List<int> data, int n, int i, ref int k, ref int minValue)
+        private static bool IsSorted(List<int> data)
         {
-            for (int j = i + 1; j < n; j++)
+            for (int i = 1; i < data.Count; i++)
             {
-                if (data[j] < minValue)
+                if (data[i - 1] > data[i])
                 {
-                    k = j;
-                    minValue = data[j];
+                    Console.WriteLine("fault indexes: " + (i - 1) + " " + i);
+                    return false;
                 }
             }
+
+            return true;
         }
         private static void PrintResultMessage(List<int> data)
         {
@@ -175,21 +155,6 @@ namespace Lab2aads
                 Console.WriteLine("Oops.. I'm affraid it's not sorted, sir.");
             }
         }
-        //private static void GetTimeDifference(Stopwatch stopWatch)
-        //{
-        //    int timeSecond, timeMillisecond;
-        //    timeSecond = DateTime.Now.Second - beginTimeSecond;
-        //    timeMillisecond = DateTime.Now.Millisecond - beginTimeMillisecond;
-
-        //    if (timeMillisecond < 0)
-        //    {
-        //        timeSecond--;
-        //        timeMillisecond = 1 + timeMillisecond;
-        //    }
-
-        //    Console.WriteLine("Time: " + timeSecond.ToString() + ":" + timeMillisecond.ToString());
-        //}
         #endregion
-
     }
 }
