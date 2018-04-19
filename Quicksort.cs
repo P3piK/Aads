7d09d1acae04613352f7aa6e07931f7155f4dbc6
Pete-PC <pete@great.com>
2018-04-20 00:27:37 +0200

    Refactor lab2

---
+++ Quicksort.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3
+{
+    public class Quicksort
+    {
+        // First element
+        public static void Exec1(List<int> data, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = data[left];
+            while (i < j)
+            {
+                while (data[i] < pivot) i++;
+                while (data[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                Exec1(data, left, j);
+            }
+            if (i < right)
+            {
+                Exec1(data, i, right);
+            }
+        }
+
+        // Random
+        public static void Exec2(List<int> data, int left, int right)
+        {
+            Random random = new Random();
+            int i = left;
+            int j = right;
+            int pivot = data[random.Next(left, right + 1)];
+            while (i < j)
+            {
+                while (data[i] < pivot) i++;
+                while (data[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                Exec2(data, left, j);
+            }
+            if (i < right)
+            {
+                Exec2(data, i, right);
+            }
+        }
+
+        // Median of three
+        public static void Exec3(List<int> data, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = median(left, (int)(left+right)/2, right);
+            while (i < j)
+            {
+                while (data[i] < pivot) i++;
+                while (data[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                Exec3(data, left, j);
+            }
+            if (i < right)
+            {
+                Exec3(data, i, right);
+            }
+        }
+
+        private static int median(int left, int mid, int right)
+        {
+            List<int> values = new List<int>() { left, mid, right };
+            int max = values[0];
+            foreach(var val in values)
+            {
+                if(max < val)
+                {
+                    max = val;
+                }
+            }
+            values.Remove(max);
+            int res = values[0] > values[1] ? 
+                values[0] :
+                values[1];
+
+            return res;
+        }
+
+
+        #region private
+        private static void Swap(List<int> list, int indexA, int indexB)
+        {
+            int tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+        #endregion
+    }
+}
---
+++ Shellsort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3
+{
+    public class Shellsort
+    {
+        public static void Exec(List<int> data, int k)
+        {
+            int h = 1;
+            int n = data.Count;
+
+            while (h < n / Math.Pow(k, 2.0))
+            {
+                h = k * h + 1;
+            }
+            while (h > 0)
+            {
+                for (int i = h; i < n; i++)
+                {
+                    int x = data[i];
+                    int j = i;
+                    while (j >= h && x < data[j - h])
+                    {
+                        data[j] = data[j - h];
+                        j -= h;
+                    }
+                    data[j] = x;
+                }
+                h = h / k;
+            }
+        }
+    }
+}
