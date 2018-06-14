a4663cf365f36756341ec69235895e390d5034f8
P3piK <grytpiotr@gmail.com>
2018-06-14 22:57:18 +0200

    Add choosing if should be loaded from file

--- Compression/Compression/Program.cs
+++ Compression/Compression/Program.cs
@@ -7,16 +7,20 @@ namespace Compression
 {
     public class Program
     {
-        public static string WORD_TO_COMPRESS;
+        public static string WORD_TO_COMPRESS = "ABABCDABCABCDCADABCA";
         public const int BUFFER_LENGTH = 4;
         public const int DICTIONARY_LENGTH = 8;
         public const string READ_FILE_NAME = "ToCompress.txt";
         public const string WRITE_FILE_NAME = "Compressed.txt";
+        public const bool READ_WRITE_TO_FILE = true;
 
 
         static void Main(string[] args)
         {
-            WORD_TO_COMPRESS = FileAssistant.ReadFile(READ_FILE_NAME);
+            if (READ_WRITE_TO_FILE)
+            {
+                WORD_TO_COMPRESS = FileAssistant.ReadFile(READ_FILE_NAME);
+            }
 
             var compressionDto = new CompressionDto();
             var decompressionDto = new DecompressionDto();
@@ -26,7 +30,7 @@ namespace Compression
             CompressionController.PerformCompression(compressionDto, bufferOffset);
 
             View.PrintStartDecompressionMessage();
-            DecompressionController.PerformDecompression(compressionDto, true);
+            DecompressionController.PerformDecompression(compressionDto, READ_WRITE_TO_FILE);
             
         }
     }
